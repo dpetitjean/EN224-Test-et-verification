416a523ba301e28feba32922d231f6f4597e7b84
Dimitri Petitjean <dimitri.ptjn@gmail.com>
2021-02-12 13:26:40 +0100

    Ensemble des dossiers software et hardware réalisés

--- 1_Software/Etape_1/src/main.c
+++ 1_Software/Etape_1/src/main.c
@@ -4,14 +4,25 @@
 
 int PGCD(int A, int B)
 {
-
+	while(A != B){
+		if(A > B){
+			A = A-B;
+		}else{
+			B = B-A;
+		}
+	}
 	return A;
 }
 
 int main (int argc, char * argv []){
-	printf("(II) Starting PGCD program\n");
 
+	int A = atoi(argv[1]);
+	int B = atoi(argv[2]);
 
-	printf("(II) End of PGCD program\n");
+	int pgcd = PGCD(A, B);
+
+	printf("Le PGCD de %d et %d est %d\n", A, B, pgcd);
+
+	
   return 0;
 }
